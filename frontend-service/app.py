d69d5887f191cec2ab67a63a566c567538877e80
Dev Shinde <shindedev123@gmail.com>
2024-12-01 03:22:14 +0530

    message

--- frontend-service/app.py
+++ frontend-service/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template,jsonify  
+from flask import Flask, render_template, jsonify
 import os
 from dotenv import load_dotenv
 
@@ -14,4 +14,4 @@ def home():
     return render_template('home.html')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5001)
+    app.run(host='0.0.0.0', port=5001, debug=True)
